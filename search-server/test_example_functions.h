2a2997d8407e40f1264700fec183969a37877a0f
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-06-05 13:30:14 +0300

    Update test_example_functions.h

--- search-server/test_example_functions.h
+++ search-server/test_example_functions.h
@@ -1,5 +1,1 @@
-#pragma once
-#include "search_server.h"
-using namespace std;
- 
-void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
+
