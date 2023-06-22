db52deee7a0d01eab971f2dd9320f391144e1c21
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-06-22 17:09:58 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,1 +1,13 @@
-// в качестве заготовки кода используйте последнюю версию своей поисковой системы
+#include "document.h"
+
+using namespace std::string_literals;
+
+
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+    out << "{ document_id = "s << document.id 
+        << ", relevance = "s << document.relevance 
+        << ", rating = "s << document.rating << " }"s;
+    return out;
+}
+ 
+
