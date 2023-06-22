9589615403506f61f05c91aa779f874f60a2f7ff
aleksandrchepinoga18 <113390482+aleksandrchepinoga18@users.noreply.github.com>
2023-06-22 17:17:12 +0300

    Update search_server.h

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -11,11 +11,12 @@
 #include <vector>
 #include <type_traits>
 #include <random>
-#include <future>  
+#include <future> 
+
 #include "concurrent_map.h"
-#include "read_input_functions.h"
 #include "string_processing.h"
 #include "log_duration.h" 
+#include "document.h"
  
 using namespace std::string_literals;
  
@@ -24,15 +25,14 @@ public:
     template <typename StringContainer>
     SearchServer(const StringContainer& stop_words);
     SearchServer(const std::string& stop_words_text) : SearchServer(SplitIntoWords(stop_words_text)){}
-    SearchServer(std::string_view& stop_words_text) : SearchServer(SplitIntoWords(stop_words_text)){}
+    SearchServer(std::string_view stop_words_text) : SearchServer(SplitIntoWords(stop_words_text)){}
     SearchServer() = default;
     
     void AddDocument(int document_id, 
                      std::string_view document,
                      DocumentStatus status, 
                      const std::vector<int>& ratings);
-    
-  
+      
   template <typename DocumentPredicate>
 std::vector<Document> FindTopDocuments(std::string_view raw_query,DocumentPredicate document_predicate) const;
     
@@ -48,16 +48,7 @@ std::vector<Document> FindTopDocuments(std::string_view raw_query, DocumentStatu
 std::vector<Document> FindTopDocuments(ExecutionPolicy&& policy, std::string_view raw_query) const;
  
 std::vector<Document> FindTopDocuments(std::string_view raw_query) const;  
-
-
-
-
-
-
-    
   
-    
-    
     int GetDocumentCount() const;
     
     std::vector<int>::const_iterator begin() const;
@@ -111,16 +102,16 @@ private:
         bool is_stop;
     };
  
-    QueryWord ParseQueryWord(std::string_view& text) const;
+    QueryWord ParseQueryWord(std::string_view text) const;
  
     struct Query {
         std::vector<std::string_view> plus_words;
         std::vector<std::string_view> minus_words;
     };
  
-    Query ParseQuery(std::string_view& text) const;
+    Query ParseQuery(std::string_view text) const;
         
-    double ComputeWordInverseDocumentFreq(std::string_view& word) const;
+    double ComputeWordInverseDocumentFreq(std::string_view word) const;
  
     template <typename DocumentPredicate>
     std::vector<Document> FindAllDocuments(const Query& query, 
@@ -141,9 +132,6 @@ SearchServer::SearchServer(const StringContainer& stop_words) : stop_words_(Make
         throw std::invalid_argument("Some of stop words are invalid"s);
     }
 }
- 
-
-
 
 template <typename DocumentPredicate>
 std::vector<Document> SearchServer::FindTopDocuments(std::string_view raw_query,DocumentPredicate document_predicate) const {
@@ -185,19 +173,11 @@ std::vector<Document> SearchServer::FindTopDocuments(ExecutionPolicy &&policy,st
                            );
 }
     
-   template <typename ExecutionPolicy>
+template <typename ExecutionPolicy>
 std::vector<Document> SearchServer::FindTopDocuments(ExecutionPolicy&& policy, std::string_view raw_query) const {
     return FindTopDocuments(policy, raw_query, DocumentStatus::ACTUAL);
 }
- 
 
-
-
-
-
-
-
- 
 template <typename DocumentPredicate>
 std::vector<Document> SearchServer::FindAllDocuments(const Query& query, 
                                                      DocumentPredicate document_predicate) const {
@@ -255,7 +235,7 @@ std::vector<Document> SearchServer::FindAllDocuments(const std::execution::paral
     const auto plus_func = [this, 
                        &document_predicate, 
                        &document_to_relevance] (std::string_view word) {
-                           if (word_to_document_freqs_.count(word) == 0) {//
+                           if (word_to_document_freqs_.count(word) == 0) {
                                 return;
                            }
                            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
